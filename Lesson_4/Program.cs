677f35b77279c0913e4bf9220d243c50bb44a9e1
Ivan Pogulyaka <dandeliongod95@gmail.com>
2019-08-22 21:31:19 +0300

    lesson_5

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+	class Program
+	{
+		public static bool IsPolindrome(string value)
+		{
+			for (int i = 0; i < value.Length / 2; i++)
+			{
+				if (value[i] != value[value.Length - 1])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		//private char[] GetRandomArray(int count)
+		//{
+		//	Random r = new Random();
+		//	char[] result = new char;
+		//}
+
+		//public char[] Sort(char[] value)
+		//{
+
+		//}
+
+		static void Main(string[] args)
+		{
+			//int[] array = new int[] { 8, 2, 3, 4, 6 };
+			char[] array = new char[] { '8', '2', '3', '4', '6' };
+
+			SortCharBubble(array);
+			//SortIntBubble(array);
+			//Console.WriteLine(IsPolindrome("qwerewq"));
+			Console.ReadKey();
+		}
+
+		//private static void SortIntBubble(int[] array)
+		//{
+		//	for (int i = 0; i < array.Length - 1; i++)
+		//	{
+		//		if (array[i] > array[i + 1])
+		//		{
+		//			int temp = array[i];
+		//			array[i] = array[i + 1];
+		//			array[i + 1] = temp;
+		//		}
+		//	}
+
+		//	foreach (var item in array)
+		//	{
+		//		Console.WriteLine(item);
+		//	}
+
+		private static void SortCharBubble(char[] array)
+		{
+			for (int i = 0; i < array.Length - 1; i++)
+			{
+				if (array[i] > array[i + 1])
+				{
+					char temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+				}
+			}
+
+			foreach (var item in array)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
---
+++ Lesson_5_OOP/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_OOP
+{
+	class Human
+	{
+
+	}
+
+	class Cat
+	{
+		public string Name;
+		private int _age;
+		public Cat Father;
+		public Cat Mother;
+		public Human Owner;
+
+		public int Age
+		{
+			get { return _age; }
+			set { if (value >= 0) Age = value; }
+		}
+		
+
+		public void Voice()
+		{
+			Console.WriteLine($"Meou! My name is {Name}");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Cat catBob = new Cat();
+			catBob.Name = "Bob";
+			Cat catJohn = new Cat();
+			catJohn.Name = "John";
+			catJohn.Age = 1;
+			catBob.Voice();
+			catJohn.Voice();
+
+			Console.ReadKey();
+		}
+	}
+}
