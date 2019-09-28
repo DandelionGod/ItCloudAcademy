f00346587560abb2d419835c5be7156518b08673
Ivan Pogulyaka <dandeliongod95@gmail.com>
2019-09-28 13:06:36 +0300

    cmd

--- Lesson_20_Exceptions/Program.cs
+++ Lesson_20_Exceptions/Program.cs
@@ -6,24 +6,71 @@ using System.Threading.Tasks;
 
 namespace Lesson_20_Exceptions
 {
+	class AuthException : Exception
+	{
+		public AuthException();
+	}
+
+	class User
+	{
+		public string Login { get; set; }
+		public string Password { get; set; }
+
+		internal void Init(string[] args)
+		{
+			Login = args[0].Split(':')[1];
+			Password = args[1].Split(':')[1];
+		}
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			try
+			User currentUser = new User() { Login = "log", Password = "pwd" };
+			User incomeUser = new User();
+			incomeUser.Init(args);
+
+			if (currentUser.Password.Equals(incomeUser.Password) && currentUser.Login.Equals(incomeUser.Login))
 			{
-				throw new IndexOutOfRangeException();
+				Console.WriteLine("Looks good");
 			}
-			catch(IndexOutOfRangeException ex)
+			else
 			{
-				Console.WriteLine(ex.StackTrace);
-				Console.WriteLine(ex.Message);
+				throw new AuthException("wrong login/password");
 			}
-			catch(SystemException se)
-			{
-				Console.WriteLine(se.Message);
-			}
-			Console.ReadKey();
-		}
+
+		//	try
+		//	{
+		//		Method();
+		//		//throw new IndexOutOfRangeException();
+		//	}
+		//	catch(IndexOutOfRangeException ex)
+		//	{
+		//		Console.WriteLine(ex.StackTrace);
+		//		Console.WriteLine(ex.Message);
+		//	}
+		//	catch(SystemException se)
+		//	{
+		//		Console.WriteLine(se.Message);
+		//	}
+		//	Console.ReadKey();
+		//}
+
+		//private static void Method()
+		//{
+		//	try
+		//	{
+		//		throw new IndexOutOfRangeException("Exception messege");
+		//	}
+		//	catch(SystemException se)
+		//	{
+		//		Console.WriteLine(se.Message);
+		//	}
+		//	throw new IndexOutOfRangeException();
+		//}
+		//private static void MethodInner()
+		//{
+		//	throw new AuthException();
+		//}
 	}
 }
---
+++ Lesson_21_IO/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson_21_IO
+{
+	class Program
+	{
+		static string basePath = @"C:\Users\Student\source\repos\ItCloudAcademy\Lesson_21_IO\";
+		static void Main(string[] args)
+		{
+			//	byte[] _bytes = Encoding.ASCII.GetBytes("new Test string");
+
+			//	MemoryStream ms = new MemoryStream(30);
+			//	{
+			//		StreamWriter sw = new StreamWriter(ms);
+			//		{
+			//			sw.Write("new Test string");
+			//			sw.Flush();
+			//		}
+			//		ms.Position = 0;
+
+			//		StreamReader sr = new StreamReader(ms);
+			//		{
+
+			//			Console.WriteLine(sr.ReadLine());
+			//		}
+			//	}
+
+			//	FileStream fileStream = new FileStream(@"C:\Users\Student\source\repos\ItCloudAcademy\Lesson_21_IO\testFileStream.txt", FileMode.Create);
+			//	fileStream.Write(_bytes, 0, _bytes.Length);
+			//	//fileStream.Flush();
+
+			//using (MemoryStream memoryStream = new MemoryStream(@"C:\Users\Student\source\repos\ItCloudAcademy\Lesson_21_IO\testFileStream.txt", FileMode.Create))
+			//	fileStream.Write(_bytes, 0, _bytes.Length);
+			////fileStream.Flush();
+
+			new FolderView().Show();
+
+			Console.ReadKey();
+		}
+	}
+
+	class FolderView
+	{
+		string _basePath;
+		public FolderView(string path = @"C:\Users\Student\source\repos\ItCloudAcademy\Lesson_21_IO\")
+		{
+			_basePath = path;
+		}
+
+		public void Show()
+		{
+			var dirs = Directory.GetDirectories(_basePath).Select(s => new DirectoryInfo(s)).Cast<FileSystemInfo>();
+			dirs.Concat(Directory.GetFiles(_basePath).Select(s => new FileInfo(s)).Cast<FileSystemInfo>());
+			foreach(var item in dirs)
+			{
+				Console.WriteLine(item.Name);
+			}
+
+
+			//String mypath = _basePath;
+			//Directory
+			//	.GetFiles(mypath, "*", SearchOption.AllDirectories)
+			//	.ToList()
+			//	.ForEach(f => Console.WriteLine(Path.GetFileName(f)));
+
+			//foreach (var item in Directory.GetDirectories(_basePath))
+			//{
+			//	Console.WriteLine(item);
+			//}
+
+			//foreach (var item in Directory.GetFiles(_basePath))
+			//{
+			//	Console.WriteLine(item);
+			//}
+		}
+	}
+}
