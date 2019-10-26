5065471209c0f87a9c95babfe8e4faac894819be
Ivan Pogulyaka <dandeliongod95@gmail.com>
2019-10-26 11:33:06 +0300

    qwe

---
+++ Lesson_29_EntityFramework/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_29_EntityFramework
+{
+	class ApplicationContext : DbContext
+	{
+		public ApplicationContext() : base("EntityTest")
+		{
+			Database.Log += Console.WriteLine;
+		}
+		public DbSet<File> Files { get; set; }
+	}
+}
---
+++ Lesson_29_EntityFramework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_29_EntityFramework
+{
+	class File
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public byte[] Data { get; set; }
+		public long Size { get; set; }
+		public int ParentId { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var context = new ApplicationContext())
+			{
+				context.Database.Initialize(true);
+			}
+		}
+	}
+}
